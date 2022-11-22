c7d2d2758e1bb610930184a4ca2a5774f6337637
Dairo <dairoroberto2014@gmail.com>
2022-11-22 12:16:05 -0500

    Funcionalidades de Instituciones

---
+++ src/Controller/Postgrado/ProgramasAprobadosController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller\Postgrado;
+
+use App\Entity\Postgrado\SolicitudPrograma;
+use App\Entity\Postgrado\SolicitudProgramaComision;
+use App\Entity\Security\User;
+use App\Form\Postgrado\AprobarProgramaType;
+use App\Form\Postgrado\CambioEstadoProgramaType;
+use App\Form\Postgrado\ComisionProgramaType;
+use App\Form\Postgrado\NoAprobarProgramaType;
+use App\Form\Postgrado\SolicitudProgramaType;
+use App\Repository\Postgrado\ComisionRepository;
+use App\Repository\Postgrado\EstadoProgramaRepository;
+use App\Repository\Postgrado\SolicitudProgramaComisionRepository;
+use App\Repository\Postgrado\SolicitudProgramaRepository;
+use App\Services\TraceService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Component\Serializer\SerializerInterface;
+
+/**
+ * @Route("/postgrado/programas_aprobados")
+ * @IsGranted("ROLE_ADMIN", "ROLE_GEST_CATDOC")
+ */
+class ProgramasAprobadosController extends AbstractController
+{
+
+    /**
+     * @Route("/", name="app_programas_aprobados_index", methods={"GET"})
+     * @param SolicitudProgramaRepository $solicitudProgramaRepository
+     * @return Response
+     */
+    public function index(SolicitudProgramaRepository $solicitudProgramaRepository)
+    {
+        return $this->render('modules/postgrado/programas_aprobados/index.html.twig', [
+            'registros' => $solicitudProgramaRepository->findBy([], ['activo' => 'desc', 'id' => 'desc']),
+        ]);
+    }
+
+}
