ff380e588cd0bc29676ecbf51c5d8bdde628004c
Dairo <dairo@dgtalliance.com>
2023-03-28 00:45:59 -0400

    Actualizando

--- src/Controller/Institucion/InstitucionController.php
+++ src/Controller/Institucion/InstitucionController.php
@@ -93,7 +93,7 @@ class InstitucionController extends AbstractController
                 $institucionRepository->add($institucion, true);
 
                 $traceService = new TraceService($requestStack, $entityManager, $serializer);
-                $traceService->registrar($this->getParameter('accion_registrar'), $this->getParameter('objeto_institucion'), null, serialize($institucion), $this->getParameter('tipo_traza_negocio'));
+                $traceService->registrar($this->getParameter('accion_registrar'), $this->getParameter('objeto_institucion'), null, $institucion, $this->getParameter('tipo_traza_negocio'));
 
                 $this->addFlash('success', 'El elemento ha sido creado satisfactoriamente.');
                 return $this->redirectToRoute('app_institucion_index', [], Response::HTTP_SEE_OTHER);
