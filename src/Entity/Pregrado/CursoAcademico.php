d9f993e74cc1ff7d7d98177275837287e4491254
Dairo <dairo@dgtalliance.com>
2023-03-31 14:01:10 -0400

    Actualizando

--- src/Entity/Pregrado/CursoAcademico.php
+++ src/Entity/Pregrado/CursoAcademico.php
@@ -2,14 +2,107 @@
 
 namespace App\Entity\Pregrado;
 
-use App\Entity\BaseNomenclator;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity
+ * @UniqueEntity(fields="nombre", message="EL valor {{ value }} ya existe. Por favor inserte otro valor.")
  * @ORM\Table(name="pregrado.tbn_curso_academico")
  */
-class CursoAcademico extends BaseNomenclator
+class CursoAcademico
 {
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    protected ?int $id;
+
+    /**
+     * @Gedmo\Timestampable(on="create")
+     * @ORM\Column(type="datetime")
+     */
+    protected $creado;
+
+    /**
+     * @Gedmo\Timestampable(on="update")
+     * @ORM\Column(type="datetime")
+     */
+    protected $actualizado;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCreado()
+    {
+        return $this->creado;
+    }
+
+    public function getActualizado()
+    {
+        return $this->actualizado;
+    }
+
+    /**
+     * @ORM\Column(type="string", nullable=false)
+     */
+    private ?string $nombre = null;
+
+    /**
+     * @ORM\Column(type="string", nullable=true)
+     * @Assert\Regex(
+     *           pattern= "/^[0-9a-zA-ZáéíóúàèìòùÀÈÌÒÙÁÉÍÓÚñÑüÜ_\s]+$/",
+     *           match=   true,
+     *           message= "Caracteres no válidos, por favor verifique."
+     * )
+     */
+    private ?string $descripcion = null;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=false)
+     */
+    private ?bool $activo = true;
+
+
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    public function getDescripcion()
+    {
+        return $this->descripcion;
+    }
+
+    public function setDescripcion($descripcion)
+    {
+        $this->descripcion = $descripcion;
+
+        return $this;
+    }
+
+    public function getActivo()
+    {
+        return $this->activo;
+    }
+
+    public function setActivo($activo)
+    {
+        $this->activo = $activo;
+
+        return $this;
+    }
 
 }
