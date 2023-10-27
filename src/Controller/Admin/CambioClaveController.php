7447a4a47c516420d71728bd60f6dcece7d5ac72
Dairo <dairo@dgtalliance.com>
2023-10-27 01:19:40 -0400

    Configuracion de seguridad

--- src/Controller/Admin/CambioClaveController.php
+++ src/Controller/Admin/CambioClaveController.php
@@ -7,9 +7,11 @@ use App\Form\Admin\CambiarClaveFormType;
 use App\Form\Admin\UserChgPswFormType;
 use App\Form\Admin\UserFormType;
 use App\Repository\Security\UserRepository;
+use Doctrine\ORM\ORMException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
@@ -30,27 +32,31 @@ class CambioClaveController extends AbstractController
      * @param UserRepository $usuarioRepository
      * @param UserPasswordEncoderInterface $encoder
      * @return Response
-     * @throws \Doctrine\ORM\ORMException
+     * @throws ORMException
      * @throws \Doctrine\ORM\OptimisticLockException
      */
     public function cambiarContrasenna(Request $request, User $usuario, UserRepository $usuarioRepository, UserPasswordEncoderInterface $encoder)
     {
-        $form = $this->createForm(CambiarClaveFormType::class);
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
+        try {
+            $form = $this->createForm(CambiarClaveFormType::class);
+            $form->handleRequest($request);
+            if ($form->isSubmitted() && $form->isValid()) {
 
-            if ($form->getData()->getPasswordPlainText() != "") {
-                $encodedPassword = $encoder->encodePassword($usuario, $form->getData()->getPasswordPlainText());
-                $usuario->setPassword($encodedPassword);
-                $usuario->setPasswordChangeFirstTime(true);
+                if ($form->getData()->getPasswordPlainText() != "") {
+                    $encodedPassword = $encoder->encodePassword($usuario, $form->getData()->getPasswordPlainText());
+                    $usuario->setPassword($encodedPassword);
+                    $usuario->setPasswordChangeFirstTime(true);
+                }
+
+                $usuarioRepository->edit($usuario, true);
+                $this->addFlash('success', 'El elemento ha sido actualizado satisfactoriamente.');
+                return $this->redirectToRoute('app_dash_board', [], Response::HTTP_SEE_OTHER);
             }
+            return $this->render('modules/admin/usuario/cambiar_clave.html.twig', [
+                'form' => $form->createView(),
+            ]);
+        } catch (\Exception | NotFoundHttpException $exception) {
 
-            $usuarioRepository->edit($usuario, true);
-            $this->addFlash('success', 'El elemento ha sido actualizado satisfactoriamente.');
-            return $this->redirectToRoute('app_dash_board', [], Response::HTTP_SEE_OTHER);
         }
-        return $this->render('modules/admin/usuario/cambiar_clave.html.twig', [
-            'form' => $form->createView(),
-        ]);
     }
 }
