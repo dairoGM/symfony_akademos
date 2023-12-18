04db23b8a33405fe01a2637857bdd94ba7004780
Dairo <dairo@dgtalliance.com>
2023-12-18 13:17:59 -0500

    Ajustes 18 de diciembre

--- src/Controller/Pregrado/SolicitudProgramaAcademicoAprobadoController.php
+++ src/Controller/Pregrado/SolicitudProgramaAcademicoAprobadoController.php
@@ -118,62 +118,75 @@ class SolicitudProgramaAcademicoAprobadoController extends AbstractController
     public function asignarUniversidad(Request $request, SolicitudProgramaAcademico $solicitudProgramaAcademico, SolicitudProgramaAcademicoInstitucionRepository $solicitudProgramaAcademicoInstitucionRepository)
     {
         try {
-        $entidad = new SolicitudProgramaAcademicoInstitucion();
-        $form = $this->createForm(SolicitudProgramaInstitucionType::class, $entidad);
-        $form->handleRequest($request);
+            $entidad = new SolicitudProgramaAcademicoInstitucion();
+            $form = $this->createForm(SolicitudProgramaInstitucionType::class, $entidad);
+            $form->handleRequest($request);
 
-        $modalidades = [];
-        if ($solicitudProgramaAcademico->getModalidadDiurno()) {
-            $item['id'] = 'diurno';
-            $item['nombre'] = 'Diurno';
-            $modalidades[] = $item;
-        }
-        if ($solicitudProgramaAcademico->getModalidadPorEncuentro()) {
-            $item['id'] = 'por_encuentro';
-            $item['nombre'] = 'Por encuentro';
-            $modalidades[] = $item;
-        }
+            $modalidades = [];
+            if ($solicitudProgramaAcademico->getModalidadDiurno()) {
+                $item['id'] = 'diurno';
+                $item['nombre'] = 'Diurno';
+                $modalidades[] = $item;
+            }
+            if ($solicitudProgramaAcademico->getModalidadPorEncuentro()) {
+                $item['id'] = 'por_encuentro';
+                $item['nombre'] = 'Por encuentro';
+                $modalidades[] = $item;
+            }
 
-        if ($solicitudProgramaAcademico->getModalidadADistancia()) {
-            $item['id'] = 'a_distancia';
-            $item['nombre'] = 'A distancia';
-            $modalidades[] = $item;
-        }
+            if ($solicitudProgramaAcademico->getModalidadADistancia()) {
+                $item['id'] = 'a_distancia';
+                $item['nombre'] = 'A distancia';
+                $modalidades[] = $item;
+            }
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $exist = $solicitudProgramaAcademicoInstitucionRepository->findBy(['solicitudProgramaAcademico' => $solicitudProgramaAcademico->getId(), 'institucion' => $request->request->all()['solicitud_programa_institucion']['institucion']]);
-            if (empty($exist)) {
-                $entidad->setSolicitudProgramaAcademico($solicitudProgramaAcademico);
-
-                if (isset($request->request->all()['modalidad'])) {
-                    if ('diurno' == $request->request->all()['modalidad']) {
-                        $entidad->setModalidadDiurno(true);
-                        $entidad->setDuracionCursoDiurno($entidad->getSolicitudProgramaAcademico()->getDuracionCursoDiurno());
-                    }
-                    if ('por_encuentro' == $request->request->all()['modalidad']) {
-                        $entidad->setModalidadPorEncuentro(true);
-                        $entidad->setDuracionCursoPorEncuentro($entidad->getSolicitudProgramaAcademico()->getDuracionCursoPorEncuentro());
-                    }
-                    if ('a_distancia' == $request->request->all()['modalidad']) {
-                        $entidad->setModalidadADistancia(true);
-                        $entidad->setDuracionCursoADistancia($entidad->getSolicitudProgramaAcademico()->getDuracionCursoADistancia());
-                    }
+            //validar tambien la modalidad
+            if ($form->isSubmitted() && $form->isValid()) {
+                $campo = "modalidadDiurno";
+                if ('por_encuentro' == $request->request->all()) {
+                    $campo = "modalidadPorEncuentro";
                 }
-                $solicitudProgramaAcademicoInstitucionRepository->add($entidad, true);
+                if ('a_distancia' == $request->request->all()) {
+                    $campo = "modalidadADistancia";
+                }
+                $exist = $solicitudProgramaAcademicoInstitucionRepository->findBy(
+                    [
+                        'solicitudProgramaAcademico' => $solicitudProgramaAcademico->getId(),
+                        'institucion' => $request->request->all()['solicitud_programa_institucion']['institucion'],
+                        "$campo" => true
+                    ]);
+                if (empty($exist)) {
+                    $entidad->setSolicitudProgramaAcademico($solicitudProgramaAcademico);
 
-                $this->addFlash('success', 'El elemento ha sido creado satisfactoriamente.');
+                    if (isset($request->request->all()['modalidad'])) {
+                        if ('diurno' == $request->request->all()['modalidad']) {
+                            $entidad->setModalidadDiurno(true);
+                            $entidad->setDuracionCursoDiurno($entidad->getSolicitudProgramaAcademico()->getDuracionCursoDiurno());
+                        }
+                        if ('por_encuentro' == $request->request->all()['modalidad']) {
+                            $entidad->setModalidadPorEncuentro(true);
+                            $entidad->setDuracionCursoPorEncuentro($entidad->getSolicitudProgramaAcademico()->getDuracionCursoPorEncuentro());
+                        }
+                        if ('a_distancia' == $request->request->all()['modalidad']) {
+                            $entidad->setModalidadADistancia(true);
+                            $entidad->setDuracionCursoADistancia($entidad->getSolicitudProgramaAcademico()->getDuracionCursoADistancia());
+                        }
+                    }
+                    $solicitudProgramaAcademicoInstitucionRepository->add($entidad, true);
+
+                    $this->addFlash('success', 'El elemento ha sido creado satisfactoriamente.');
+                    return $this->redirectToRoute('app_solicitud_programa_academico_aprobado_asignar_universidad', ['id' => $solicitudProgramaAcademico->getId()], Response::HTTP_SEE_OTHER);
+                }
+                $this->addFlash('error', 'El elemento ya existe.');
                 return $this->redirectToRoute('app_solicitud_programa_academico_aprobado_asignar_universidad', ['id' => $solicitudProgramaAcademico->getId()], Response::HTTP_SEE_OTHER);
             }
-            $this->addFlash('error', 'El elemento ya existe.');
-            return $this->redirectToRoute('app_solicitud_programa_academico_aprobado_asignar_universidad', ['id' => $solicitudProgramaAcademico->getId()], Response::HTTP_SEE_OTHER);
-        }
 
-        return $this->render('modules/pregrado/solicitud_programa_academico_aprobado/asignar_universidad.html.twig', [
-            'form' => $form->createView(),
-            'modalidades' => $modalidades,
-            'solicitudProgramaAcademico' => $solicitudProgramaAcademico,
-            'registros' => $solicitudProgramaAcademicoInstitucionRepository->findBy(['solicitudProgramaAcademico' => $solicitudProgramaAcademico->getId()])
-        ]);
+            return $this->render('modules/pregrado/solicitud_programa_academico_aprobado/asignar_universidad.html.twig', [
+                'form' => $form->createView(),
+                'modalidades' => $modalidades,
+                'solicitudProgramaAcademico' => $solicitudProgramaAcademico,
+                'registros' => $solicitudProgramaAcademicoInstitucionRepository->findBy(['solicitudProgramaAcademico' => $solicitudProgramaAcademico->getId()])
+            ]);
         } catch (\Exception $exception) {
             $this->addFlash('error', $exception->getMessage());
             return $this->redirectToRoute('app_solicitud_programa_academico_aprobado_index', [], Response::HTTP_SEE_OTHER);
