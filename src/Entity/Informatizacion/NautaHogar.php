93851d415ed113d8e977355d8472dbb5806ed65c
Dairo <dairo@dgtalliance.com>
2024-07-31 23:29:20 -0400

    Ajustes 30 / 07

--- src/Entity/Informatizacion/NautaHogar.php
+++ src/Entity/Informatizacion/NautaHogar.php
@@ -6,11 +6,12 @@ use App\Entity\BaseEntity;
 use App\Entity\Personal\Persona;
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Mapping\Annotation as Gedmo;
-
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity
  * @ORM\Table(name="informatizacion.tbd_nauta_hogar")
+ * @UniqueEntity(fields="numeroTelefono", message="EL valor {{ value }} ya existe. Por favor inserte otro valor.")
  */
 class NautaHogar extends BaseEntity
 {
